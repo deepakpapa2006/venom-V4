9de49d6cfc9fd9fcb65b3fd83a99ded924474fdd
deepakpapa2006 <deepakpapa2006@gmail.com>
2024-07-21 18:37:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7491316970:AAHRVirXetcOzt8mgcbr9D26cimwk5HKmwY")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("deepakbisht360")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5595968481")
 
 
 
