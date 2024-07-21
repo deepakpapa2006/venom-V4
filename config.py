f78e37d16c9dc3af01a1537e666e85e358e0d30e
deepakpapa2006 <deepakpapa2006@gmail.com>
2024-07-21 18:59:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 BOT_TOKEN=("7491316970:AAHRVirXetcOzt8mgcbr9D26cimwk5HKmwY")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("deepakdaddy")
+OWNER_USERNAME=("deepakbisht360")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("5595968481")
